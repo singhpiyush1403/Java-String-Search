6a4823d4045ff0282db57637f66c5060b81f5e2c
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-03-16 19:27:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Java is powerful";
+        
+        System.out.println(str.contains("Java")); 
+        System.out.println(str.startsWith("Java")); 
+        System.out.println(str.endsWith("powerful")); 
+    }
+}
